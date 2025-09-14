e169786f359d1fc968e4bc926e0a64558b454b98
details404 <natalialuin3888@gmail.com>
2025-09-15 00:13:41 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,60 @@
+if(2 === 1 + 1){
+  setTimeout(() =>{
+  document.querySelector(".cir").innerHTML = `void
+  <p/>loading`;}, 3000)
+}
+var data = function(){
+function x(){
+  const x = "text";
+  console.log(x);
+}; x();
+}
+var loop = {
+  y : "200"
+}
+console.log(loop);
+
+data();
+log: function y() {
+  const act = alert("data");
+  switch(act){
+    case 'test':
+      console.log("loop");
+      break;
+  }
+}
+
+
+v: function a(act){
+  c = 0;
+  switch(act){
+    case 'y':
+      c.y("200");
+      break;
+  }
+}
+a()
+
+log: function dex(){
+const ht = document.querySelector(".ht");
+ht.style.border = "#000 solid 2px";
+ht.style.height = "20px";
+ht.style.width = "20px";
+dat = ht;
+m.reset();}
+
+var da = {
+  reset: function(){
+
+dat.fillStyle = "#ccc";
+dat.font = "bold";
+dat.innerText = "data";}
+}
+  
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const image = new Image();
+image.src = "test.jpg";
+image.addEventListener("load", () => {
+  ctx.drawImage(image, 0, 0, 100, 100);});
